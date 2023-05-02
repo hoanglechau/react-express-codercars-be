6cd7defed42b463ac449858d02ebcea614a27d13
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-05-02 12:41:31 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
+const indexRouter = require("./routes/index");
+const mongoose = require("mongoose");
+require("dotenv/config");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
-
-app.use(logger('dev'));
+app.use(cors());
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Connect to MONGODB
+mongoose.connect(process.env.MONGO_URI, () => {
+  console.log("Connected to Database!");
+});
+
+app.use("/", indexRouter);
 
 module.exports = app;
