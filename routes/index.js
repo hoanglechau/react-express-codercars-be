74c00143d529d7aff6edc05d56af74fdecf3b9b2
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-05-02 12:41:42 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// CAR
+const carAPI = require('./car.api');
+router.use('/car', carAPI);
 
 module.exports = router;
