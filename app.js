b4b9cd5bb6e4d422002c21b9962992be6b6a332a
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-05-02 17:27:01 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,50 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
+
+// Middlewares
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
+const helmet = require("helmet");
+const notFound = require("./middlewares/not-found");
+const errorHandler = require("./middlewares/error-handler");
+
+// Routers
 const indexRouter = require("./routes/index");
-const mongoose = require("mongoose");
-require("dotenv/config");
+
+// Database
+const connectDB = require("./db/connect");
 
 const app = express();
 
 app.use(cors());
+app.use(helmet());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-// Connect to MONGODB
-mongoose.connect(process.env.MONGO_URI, () => {
-  console.log("Connected to Database!");
-});
-
+// Routes
 app.use("/", indexRouter);
+
+// 404 error
+app.use(notFound);
+
+// Error handler
+app.use(errorHandler);
+
+const port = process.env.PORT || 5000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
 
 module.exports = app;
